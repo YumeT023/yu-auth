d4e29c7e5cd5d6149f1dea5f155ee48a5d6a18b1
Yume <96466759+YumeT023@users.noreply.github.com>
2022-08-19 00:20:42 +0300

    feat: add component declaration

---
+++ src/common/components/types/Global.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ComponentWithChild } from '../../../types/ComponentWithChild';
+
+/*= === COLORS === */
+export type Color = 'primary' | 'secondary' | 'warning';
+
+/*= === WRAPPER ==== */
+export type WrapperType = ComponentWithChild & {
+  color?: Color;
+}
