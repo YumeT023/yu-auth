c68acc68b555fbcf3b12451d184f2c63a84e24ed
Yume <96466759+YumeT023@users.noreply.github.com>
2022-08-19 13:29:40 +0300

    feat: establish router in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Auth, Home } from './Pages';
+import { AuthProvider } from './providers';
+import { ProtectedRoute } from './utils/router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__main">
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route
+              index
+              element={(
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              )}
+            />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
