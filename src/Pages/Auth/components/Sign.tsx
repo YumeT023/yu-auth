25e3b50edb8e97a72999331f37f5bd5ce45f03ac
Yume <96466759+YumeT023@users.noreply.github.com>
2022-08-19 14:32:17 +0300

    feat: register & login

--- src/Pages/Auth/components/Sign.tsx
+++ src/Pages/Auth/components/Sign.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { SocialSignIn } from './SocialSignin';
 // import { useNavigate } from 'react-router-dom';
 import { Button, TextField } from '../../../common/components/Forms';
+import { useAuth } from '../../../utils/hooks';
+import { AuthError } from '../../../types/FirebaseError';
+import { decodeFirebaseError } from '../../../utils/function';
 // import { useAuthProvider } from '../../../Providers/useAuthProvider';
 
 type PayLoadType = {
@@ -12,13 +14,21 @@ type PayLoadType = {
   password: string;
 };
 
-export function Form() {
-  // const { doSignInWithEmailAndPassword } = useAuthProvider();
+export function Sign() {
+  const [sign, setSign] = useState<'in' | 'up'>('in');
+  const { logIn, signup } = useAuth();
+  const [error, setError] = useState<AuthError | null>();
   const navigate = useNavigate();
   const [payload, setPayload] = useState<PayLoadType>({
     email: '',
     password: '',
   });
+
+  useEffect(() => {
+    if (error) {
+      toast.error(decodeFirebaseError(error));
+    }
+  }, [error]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -30,17 +40,18 @@ export function Form() {
     e.stopPropagation();
 
     const { email, password } = payload;
-    try {
-      const auth = getAuth();
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate('/home');
-    } catch (error) {
-      if ((error as any).code === 'auth/wrong-password') {
-        toast.error('Please check the Password');
-      }
-      if ((error as any).code === 'auth/user-not-found') {
-        toast.error('Please check the Email');
-      }
+    if (sign === 'in') {
+      await logIn(email, password)
+        .then(() => {
+          navigate('/home');
+        })
+        .catch((authError) => setError(authError));
+    } else {
+      await signup(email, password)
+        .then(() => {
+          navigate('/home');
+        })
+        .catch((authError) => setError(authError));
     }
   };
 
@@ -66,7 +77,29 @@ export function Form() {
         onChange={handleChange}
       />
 
-      <Button label="CONNEXION" type="submit" color="secondary" className="xeno-bg" />
+      <Button
+        label={sign === 'in' ? 'LOGIN' : 'REGISTER'}
+        type="submit"
+        color="secondary"
+        className="xeno-bg"
+        expand="block"
+      />
+
+      {sign === 'in' ? (
+        <p className="no-default-btn-style">
+          don&apos;t have an account yet ?
+          <button type="button" onClick={() => setSign('up')}>
+            sign up here
+          </button>
+        </p>
+      ) : (
+        <p className="no-default-btn-style">
+          Already have an account ?
+          <button type="button" onClick={() => setSign('in')}>
+            sign in here
+          </button>
+        </p>
+      )}
 
       <SocialSignIn />
     </form>
